7e9f2da62d5e294df92fe5729d4f9c0f04cf3ad9
Quentin Carpentier <quentin.carpentier@outlook.be>
2019-06-30 23:51:17 +0200

    create-user linked to the backend API with Axios library

--- src/components/create-user.component.js
+++ src/components/create-user.component.js
@@ -1,4 +1,5 @@
 import React, { Component } from "react";
+import axios from "axios";
 
 export default class CreateUser extends Component {
   constructor(props) {
@@ -21,11 +22,15 @@ export default class CreateUser extends Component {
   onSubmit(e) {
     e.preventDefault();
 
-    const user = {
+    const newUser = {
       username: this.state.username
     };
 
-    console.log(user);
+    console.log(newUser);
+
+    axios
+      .post("http://localhost:5000/users/add", newUser)
+      .then(res => console.log(res.data));
 
     this.setState({
       username: ""
